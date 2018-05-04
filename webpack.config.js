2aea88f2f7c521ded73b58c9227a5630d6df0e70
Ademílson F. Tonato <ademilsonft@outlook.com>
2018-05-04 19:39:43 -0300

    This is where it all begins...

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.ts',
+
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.min.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: 'babel-loader'
+
+        },
+        {
+            test: /\.ts|\.tsx$/,
+            loader: "awesome-typescript-loader"
+        },]
+    }
+};
