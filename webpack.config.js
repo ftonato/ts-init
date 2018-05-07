0f46fd38434849d17d0819062abdb08239e2f5f4
Ademilson Flores Tonato <ademilson.tonato@cwi.com.br>
2018-05-07 15:46:15 -0300

    change entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
 const path = require('path');
 
 module.exports = {
-    entry: './index.ts',
+  entry: './src/index.ts',
 
-    output: {
-        path: path.resolve('dist'),
-        filename: 'bundle.min.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.js', '.json']
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            use: 'babel-loader'
-
-        },
-        {
-            test: /\.ts|\.tsx$/,
-            loader: "awesome-typescript-loader"
-        },]
-    }
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.min.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.ts|\.tsx$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
 };
